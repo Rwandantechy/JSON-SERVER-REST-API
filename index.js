beac3aca4568c51709d6d6898236ac80a6f93a8a
Rwandantechy <niyinnocent2027@gmail.com>
2023-02-22 15:06:55 +0530

    feat(index.js): add root index.js that starts  server that listens on port 8080

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
-var http = require("http");
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("Hello World!");
-    res.end();
-  })
-  .listen(8080);
-  // javascript for index.html
-const container = document.querySelector(".blogs");
-const searchForm = document.querySelector(".search");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("myData.json");
+var cors = require("cors");
 
-const renderPosts = async (term) => {
-  let uri = "http://localhost:3000/blogs?_sort=likes&_order=desc";
-  if (term) {
-    uri += `&q=${term}`;
-  }
-  const res = await fetch(uri);
-  const blogs = await res.json();
-  let template = "";
-  blogs.forEach((blog) => {
-    template += `
-            <div class="post">
-                <h1>${blog.blogtitle}</h1>
-                <p><small>${blog.likes} likes</small></p>
-                <p>${blog.blogbody.slice(0, 200)}</p>
-                <a href="/data/details.html?id=${blog.id}">Read more...</a>
-            </div>
-        `;
-  });
-  container.innerHTML = template;
-};
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  renderPosts(searchForm.term.value.trim());
+server.use(cors());
+server.use(router);
+server.listen(8080, () => {
+  console.log("Server is up and running on port 8080");
 });
-window.addEventListener("DOMContentLoaded", () => renderPosts());
-
